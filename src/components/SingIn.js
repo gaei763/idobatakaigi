8194763db8108bbcdfc5488a9408563aee692793
prjunkey <prjunkey@gmail.com>
2020-11-07 15:52:16 +0900

    save name by enter key

--- src/components/SingIn.js
+++ src/components/SingIn.js
@@ -76,6 +76,12 @@ export default function SignIn({ setName }) {
 						name="name"
 						autoFocus
 						onChange={(e) => setString(e.target.value)}
+						onKeyDown={(e) => {
+							if (e.key === "Enter") {
+								setName(string)
+								e.preventDefault()
+							}
+						}}
 					/>
 					<Button
 						type="button"
